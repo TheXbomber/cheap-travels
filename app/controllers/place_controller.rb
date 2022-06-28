e330f6ddb00b23ce50d450a807063287cfa15c8e
MarianoSum <summa.1842804@studenti.uniroma1.it>
2022-06-28 20:08:49 +0200

    prime modifiche

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -2,7 +2,7 @@ require "http"
 
 class PlaceController < ApplicationController
   def index
-    response=HTTP.get("https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q60", :params=>{:X-RapidAPI-Key=>'a1e0b78f93mshde8dafd691a0df9p199ec6jsn8521ec4e8226',:X-RapidAPI-Host=>'wft-geo-db.p.rapidapi.com'})
+    response=HTTP.get("https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q60", :params=>{:X-RapidAPI-Key=>'',:X-RapidAPI-Host=>'wft-geo-db.p.rapidapi.com'})
     results=response.parse['body']
   end
 end
