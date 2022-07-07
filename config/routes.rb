bf4b39c163aecf324dec4ee5ddd65647c4619dd6
Alessandro <alessandro.rocchi2000@gmail.com>
2022-07-07 18:39:55 +0200

    Added favourites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :reviews
   # profili
   get 'users/profile'
+  get 'users/add_to_favourites'
   devise_for :users
+  # resources :users, only: [:show, :edit, :update]
   #home
   get 'home/index'
 
@@ -15,8 +18,9 @@ Rails.application.routes.draw do
   root "home#index"
 
   get ":originplace/:checkindate/:checkoutdate/:numpersone/place/:destinationplace", to:"place#index"
-  get ":checkindate/:checkoutdate/:numpersone/place/:name", to:"place#index"
-  get ":checkindate/:checkoutdate/:numpersone/place/:name/viewmorehotels", to:"place#viewmorehotels"
+  get ":originplace/:checkindate/:checkoutdate/:numpersone/place/:destinationplace", to:"place#add_favourites"
+  # get ":checkindate/:checkoutdate/:numpersone/place/:name", to:"place#index"
+  # get ":checkindate/:checkoutdate/:numpersone/place/:name/viewmorehotels", to:"place#viewmorehotels"
   get ":originplace/:checkindate/:checkoutdate/:numpersone/place/:destinationplace/viewmorehotels", to:"place#gethotels"
   get ":originplace/:checkindate/:checkoutdate/:numpersone/place/:destinationplace/viewmoreflights", to:"place#getflights"
 
