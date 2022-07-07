0bcee87136957ffb9c10df01200484f5f09f95f8
Ilenia <ile.carboni@gmail.com>
2022-07-07 17:05:16 +0200

    Fix results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -27,52 +27,61 @@ class ResultsController < ApplicationController
 
     #TRAVELPAYOUT CHEAPEST FLIGHTS (voli più economici)
     #response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap", :params=>{:origin=>"AGP", :currency=>"EUR", :token=>"ea6f6d4a8d0b1be515fca155675970bb"})
-    response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap?origin=fco&destination=&currency=eur&token=ea6f6d4a8d0b1be515fca155675970bb")
+    response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap?origin=#{params["search"]["origin"]}&destination=&currency=eur&token=ea6f6d4a8d0b1be515fca155675970bb")
     results=JSON.parse(response)
     @flights=results["data"]
     @keys = @flights.keys
+    @numero_d = @keys.count
    
     
     
     @cheapest = Array.new(5, ["",0,0])
-    t = 0
+    inseriti = 0
     
     #ORDINA E SELEZIONA LE 5 DESTINAZIONI PIU ECONOMICHE
     @keys.each do |dest|
       num= @flights[dest].keys
+      assegnato=0
       num.each do |n|
-        @price = @flights[dest][n]["price"]
-        b=0
-        (0...5).each do |j| 
-            if b==0
-              if t<5
-                @cheapest[t] = [dest, n, @price]
-                t+=1
-                b=1
-              else 
-                if @cheapest[j][2] > @price
-                  b=1
-                  (j+1...5).reverse_each do |h|
-                    @cheapest[h]=@cheapest[h-1]
+        if assegnato==0
+          @price = @flights[dest][n]["price"]
+          puts @p  
+          a=0
+          (0...5).each do |j|
+              if a==0  
+                if inseriti<5
+                  @cheapest[inseriti] = [dest, n, @price]
+                  inseriti+=1
+                  assegnato=1
+                  a=1
+                else 
+                  if @cheapest[j][2] > @price
+                    assegnato=1
+                    a=1
+                    (j+1...5).reverse_each do |h|
+                      @cheapest[h]=@cheapest[h-1]
+                    end
+                    @cheapest[j]= [dest, n, @price]
                   end
-                  @cheapest[j]= [dest, n, @price]
                 end
               end
-            end
           end
+        end
       end
     end
 
-    #AMADEUS CITIES AND AIRPORT (OTTIENE I DATI SULLA CITTA DAL CODICE IATA)
+    #AIRLABS (OTTIENE I DATI SULLA CITTA DAL CODICE IATA)
     @cheap=@cheapest
     @locations = {}
     @count =0
     x = 0
     @cheap.each do |c|
-
+      puts @dest
       @dest = c[0]
+     
       #@n = c[1]
       #@volo= @flights[@dest][@n]
+=begin
       sleep 1
       begin
         response = amadeus.reference_data.locations.get(
@@ -81,18 +90,21 @@ class ResultsController < ApplicationController
           ).body
         results=JSON.parse(response)
         @airport = results["data"]
-       
+        
       rescue Amadeus::ResponseError => e
         raise e
       end
+=end
 
-      
+      citta=HTTP.get("https://airlabs.co/api/v9/cities?city_code=#{@dest}&api_key=06ec0991-7aff-487e-a815-9eab333258f4")
+      @airport=JSON.parse(citta)["response"]
+      puts @airport.inspect
         
       #SALVA DATI CITTA
         (0...1).each do |i|
           if @airport[i]
             
-            @city= @airport[i]["address"]["cityName"]
+            @city= @airport[i]["name"]
             #@city_noblank= @airport[i]["address"]["cityName"]
             #@city_noblank.gsub!(" ","-").to_s
             #@city2 = ["barcelona", "malaga", "london", "milan"]
@@ -103,18 +115,17 @@ class ResultsController < ApplicationController
             @photos = Unsplash::Photo.search("#{@city}-city-landscape")
             #@p = @photos[0]["urls"]["full"]
             @locations[@dest] = {}
-            @locations[@dest]["name"] = @airport[i]["address"]["cityName"]
-            @locations[@dest]["country"] = @airport[i]["address"]["countryName"]
-            @locations[@dest]["countryCode"] = @airport[i]["address"]["countryCode"]
-            @locations[@dest]["region"] = @airport[i]["address"]["regionCode"]
-            @locations[@dest]["iata"] = @airport[i]["iataCode"]
-            @locations[@dest]["geocode"] = [@airport[i]["geoCode"]["latitude"], @airport[i]["geoCode"]["longitude"]]
+            @locations[@dest]["name"] = @airport[i]["name"]
+            @locations[@dest]["countryCode"] = @airport[i]["country_code"]
+            #@locations[@dest]["region"] = @airport[i]["address"]["regionCode"]
+            @locations[@dest]["iata"] = @airport[i]["city_code"]
+            @locations[@dest]["geocode"] = [@airport[i]["lat"], @airport[i]["lng"]]
             @locations[@dest]["photo"] =@photos[rand(1)]["urls"]["full"]
             
               #@locations[@dest]["photo"] = r["results"][0]["urls"]["full"]
             
             if x==0
-              @geo_iniziale = [@airport[i]["geoCode"]["latitude"], @airport[i]["geoCode"]["longitude"]]
+              @geo_iniziale = [@airport[i]["lat"], @airport[i]["lng"]]
               x=1
             end
           end
