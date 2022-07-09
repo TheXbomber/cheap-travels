a3b8ac48770255b47c3aadea1d8ed7d47bc93b67
Alessandro <alessandro.rocchi2000@gmail.com>
2022-07-09 14:52:47 +0200

    Moderator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
   def edit
     before_action :authenticate_user!
   end
+
+  def promote_to_moderator
+    id = params[:uid]
+    @user = User.find(id)
+    @user.update(role: "moderator")
+    @user.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def demote_to_user
+    id = params[:uid]
+    @user = User.find(id)
+    @user.update(role: "user")
+    @user.save
+    redirect_back(fallback_location: root_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :reviews
   # profili
   get 'users/profile'
+  get 'users/promote_to_moderator'
+  get 'users/demote_to_user'
   get 'place/add_to_favourites'
   get 'place/remove_from_favourites'
   devise_for :users
