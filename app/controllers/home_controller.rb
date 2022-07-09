8df6a4f1df30de3f611258e26028b9fc7aefaccc
Ilenia <ile.carboni@gmail.com>
2022-07-09 13:51:48 +0200

    Modifica destinazioni con select

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,21 @@ class HomeController < ApplicationController
     config.utm_source = "cheap_travels_app"
   end
 
+  def airlabs_cities()
+    #AIRLABS (OTTIENE I DATI DI TUTTE LE CITTA)
+    airlabs_c=HTTP.get("https://airlabs.co/api/v9/cities?api_key=06ec0991-7aff-487e-a815-9eab333258f4")
+    @cities=JSON.parse(airlabs_c)["response"]
+  end
+
+  def cities_for_select
+    airlabs_cities()
+    @cities_select = @cities.map {|c| [ c['name'], c['country_code'] ] }
+  end
+
+
   def index 
+
+    cities_for_select
 
     @home_cities = [["Roma", "AGP"], ["Malaga", "AGP"], ["Milano", "LIN"], ["Parigi", "ORY"]]
     @photo_home = ["", "", "", ""]
