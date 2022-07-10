497345d427e72c43250a2f64757133e413603388
Ilenia <ile.carboni@gmail.com>
2022-07-10 17:53:25 +0200

    Link destinazioni home + parametri place

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  get ":originplace/:checkindate/:checkoutdate/:numpersone/:destinationplace", to:"place#index"
+  get ":originplace/:checkindate/:checkoutdate/:numpersone/:destcountry/:destiata/:destinationplace", to:"place#index"
   get ":originplace/:checkindate/:checkoutdate/:numpersone/:destinationplace/viewmorehotels", to:"place#gethotels"
   get ":originplace/:checkindate/:checkoutdate/:numpersone/:destinationplace/viewmoreflights", to:"place#getflights"
   get ":destinationplace/viewmorereviews", to:"place#getreviews"
