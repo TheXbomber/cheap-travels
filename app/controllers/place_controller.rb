a946a261f5aff67dd211791a7bdce379bc4b086f
Ilenia <ile.carboni@gmail.com>
2022-07-12 20:42:56 +0200

    Fix modifica ricerca results

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -124,10 +124,10 @@ class PlaceController < ApplicationController
     end
 
     #CHIAMA LA FUNZIONE CHE GLI RESTITUISCE UN ARRAY DI 20 HOTEL
-    #@hotels=gethotels
+    @hotels=gethotels
 
     #CHIAMA LA FUNZIONE CHE GLI RESTITUISCE I VOLI
-    #getflights
+    getflights
   end
 
   def wikidataid (place)
