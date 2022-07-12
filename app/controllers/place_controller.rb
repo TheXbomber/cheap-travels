c51134af145819ebc620bb69a763a5f445ed7803
Ilenia <ile.carboni@gmail.com>
2022-07-12 16:31:37 +0200

    Merge

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -113,14 +113,6 @@ class PlaceController < ApplicationController
 
     #PRENDE L'IMMAGINE DELLA CITTà
     begin
-<<<<<<< HEAD
-      @photo_unsplash = Unsplash::Photo.search("#{@destinationplace}-city-landscape")
-      if (@photo_unsplash[0] ==nil) 
-        @messaggeimage="Non sono state trovate immagini"
-      end
-      @imageurl= @photo_unsplash[rand(2)]["urls"]["regular"]
-      puts @imageurl
-=======
       #response=HTTP.get("https://pixabay.com/api/", :params=>{:key=>"28482200-fa6da61f3cb68d66c0df9caf9", :q=>"#{@destinationplace} city landscape", :lang=>"en", :category=>"places", :safesearch=>"true", :per_page=>"3"})
       #results=JSON.parse(response)
       #if results["total"]<1
@@ -129,7 +121,6 @@ class PlaceController < ApplicationController
       #@imageurl=results["hits"][0]["largeImageURL"]
       @photos = Unsplash::Photo.search("#{@destinationplace}-city-landscape")
       @imageurl=@photos[rand(2)]["urls"]["regular"]
->>>>>>> fd768f9298af7b4e56103f2181c4ca38b156f85a
     rescue
       @messaggeimage="C'è stato un errore nel caricamento dell'immagine"
       return
