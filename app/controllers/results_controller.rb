cd16e93358ba7a476a501a138795118fb4f8f5fd
Ilenia <ile.carboni@gmail.com>
2022-07-12 18:56:04 +0200

    Dropdown città + gestione errori voli

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,15 +6,26 @@ require "http"
 class ResultsController < ApplicationController
 
   def cheapest_flights(origin, departure)
-    #TRAVELPAYOUT CHEAPEST FLIGHTS (voli più economici)
-    #response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap", :params=>{:origin=>"AGP", :currency=>"EUR", :token=>"ea6f6d4a8d0b1be515fca155675970bb"})
-    response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap?origin=#{origin}&depart_date=#{departure}&currency=eur&token=#{Rails.application.credentials.TRAVELPAYOUTS_KEY}")
-    results=JSON.parse(response)
-    @flights=results["data"]
-    @keys = @flights.keys
-    @numero_d = @keys.count
-    @cheapest = Array.new()
-    @arrival_departure = Array.new()
+    @flights_not_found=false
+    begin
+      #TRAVELPAYOUT CHEAPEST FLIGHTS (voli più economici)
+      #response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap", :params=>{:origin=>"AGP", :currency=>"EUR", :token=>"ea6f6d4a8d0b1be515fca155675970bb"})
+      response=HTTP.get("https://api.travelpayouts.com/v1/prices/cheap?origin=#{origin}&depart_date=#{departure}&currency=eur&token=#{Rails.application.credentials.TRAVELPAYOUTS_KEY}")
+      results=JSON.parse(response)
+    rescue
+      @flights_not_found = true
+      return
+    else
+      @flights=results["data"] 
+      if @flights == nil
+        @flights_not_found = true
+      else
+        @keys = @flights.keys
+        @numero_d = @keys.count
+        @cheapest = Array.new()
+        @arrival_departure = Array.new()    
+      end  
+    end
   end
 
   def airlabs_iata(iata_code)
@@ -40,84 +51,86 @@ class ResultsController < ApplicationController
     end
     
     cheapest_flights(params["search"]["origin"], params["search"]["departure"])
-    inseriti = 0
-    #ORDINA E SELEZIONA LE 5 DESTINAZIONI PIU ECONOMICHE
-    @keys.each do |dest|
-      num= @flights[dest].keys
-      assegnato=0
-      num.each do |n|
-        if assegnato==0
-          @price = @flights[dest][n]["price"]
-          if @price < params["search"]["budget"].to_i
-            a=0
-            (0...5).each do |j|
-                if a==0  
-                  if inseriti<5
-                    @cheapest.append([dest, n, @price])
-                    inseriti+=1
-                    assegnato=1
-                    a=1
-                  else 
-                    if @cheapest[j][2] > @price
+    if @flights_not_found==false
+      inseriti = 0
+      #ORDINA E SELEZIONA LE 5 DESTINAZIONI PIU ECONOMICHE
+      @keys.each do |dest|
+        num= @flights[dest].keys
+        assegnato=0
+        num.each do |n|
+          if assegnato==0
+            @price = @flights[dest][n]["price"]
+            if @price < params["search"]["budget"].to_i
+              a=0
+              (0...5).each do |j|
+                  if a==0  
+                    if inseriti<5
+                      @cheapest.append([dest, n, @price])
+                      inseriti+=1
                       assegnato=1
                       a=1
-                      (j+1...5).reverse_each do |h|
-                        @cheapest[h]=@cheapest[h-1]
+                    else 
+                      if @cheapest[j][2] > @price
+                        assegnato=1
+                        a=1
+                        (j+1...5).reverse_each do |h|
+                          @cheapest[h]=@cheapest[h-1]
+                        end
+                        @cheapest[j]= [dest, n, @price]
                       end
-                      @cheapest[j]= [dest, n, @price]
                     end
                   end
-                end
+              end
             end
           end
         end
       end
-    end
 
-    @cheap=@cheapest  
-    @locations = {}
-    @count =0
-    x = 0
+      @cheap=@cheapest  
+      @locations = {}
+      @count =0
+      x = 0
 
-    @cheap.each do |c|
-      @dest = c[0]
-      #arrival= @flights[c[0]][c[1]]['departure_at'].to_s.split('T')
-      #departure = @flights[c[0]][c[1]]['arrival_at'].to_s.split('T')
-      #@arrival_departure.append([arrival.first, departure.first])
-      #puts @arrival_departure
-      airlabs_iata(@dest)   
-    
-        
-      #SALVA DATI CITTA
-        (0...1).each do |i|
-          if @airport[i]
-            
-            @city= @airport[i]["name"]
-            #@city_noblank= @airport[i]["address"]["cityName"]
-            #@city_noblank.gsub!(" ","-").to_s
-            #@city2 = ["barcelona", "malaga", "london", "milan"]
+      @cheap.each do |c|
+        @dest = c[0]
+        #arrival= @flights[c[0]][c[1]]['departure_at'].to_s.split('T')
+        #departure = @flights[c[0]][c[1]]['arrival_at'].to_s.split('T')
+        #@arrival_departure.append([arrival.first, departure.first])
+        #puts @arrival_departure
+        airlabs_iata(@dest)   
+      
+          
+        #SALVA DATI CITTA
+          (0...1).each do |i|
+            if @airport[i]
+              
+              @city= @airport[i]["name"]
+              #@city_noblank= @airport[i]["address"]["cityName"]
+              #@city_noblank.gsub!(" ","-").to_s
+              #@city2 = ["barcelona", "malaga", "london", "milan"]
 
-            #unspl=HTTP.get("https://api.unsplash.com/search/photos?query=malaga&per_page=1&orientation=landscape&page=1?&client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15&ar=9:3&fit=fill&fill=solid&fill-color=orange")
-            #r=JSON.parse(unspl)
+              #unspl=HTTP.get("https://api.unsplash.com/search/photos?query=malaga&per_page=1&orientation=landscape&page=1?&client_id=6fa91622109e859b1c40218a5dead99f7262cf4f698b1e2cb89dd18fc5824d15&ar=9:3&fit=fill&fill=solid&fill-color=orange")
+              #r=JSON.parse(unspl)
 
-            @photos = Unsplash::Photo.search("#{@city}-city-landscape")
-            @locations[@dest] = {}
-            @locations[@dest]["name"] = @airport[i]["name"]
-            @locations[@dest]["countryCode"] = @airport[i]["country_code"]
-            #@locations[@dest]["region"] = @airport[i]["address"]["regionCode"]
-            @locations[@dest]["iata"] = @airport[i]["city_code"]
-            @locations[@dest]["geocode"] = [@airport[i]["lat"], @airport[i]["lng"]]
-            @locations[@dest]["photo"] =@photos[rand(2)]["urls"]["regular"]
-            
-            #@locations[@dest]["photo"] = r["results"][0]["urls"]["full"]
-            
-            if x==0
-              @geo_iniziale = [@airport[i]["lat"], @airport[i]["lng"]]
-              x=1
+              @photos = Unsplash::Photo.search("#{@city}-city-landscape")
+              @locations[@dest] = {}
+              @locations[@dest]["name"] = @airport[i]["name"]
+              @locations[@dest]["countryCode"] = @airport[i]["country_code"]
+              #@locations[@dest]["region"] = @airport[i]["address"]["regionCode"]
+              @locations[@dest]["iata"] = @airport[i]["city_code"]
+              @locations[@dest]["geocode"] = [@airport[i]["lat"], @airport[i]["lng"]]
+              @locations[@dest]["photo"] =@photos[rand(2)]["urls"]["regular"]
+              
+              #@locations[@dest]["photo"] = r["results"][0]["urls"]["full"]
+              
+              if x==0
+                @geo_iniziale = [@airport[i]["lat"], @airport[i]["lng"]]
+                x=1
+              end
             end
           end
+          @count +=1
         end
-        @count +=1
       end
     end
   end
