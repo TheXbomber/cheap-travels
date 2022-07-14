c6658b7b32eda43820a08c84a4f3c42b139a6748
MarianoSum <summa.1842804@studenti.uniroma1.it>
2022-07-14 18:52:16 +0200

    test

---
+++ spec/user_spec.rb
@@ -0,0 +1,17 @@
+require 'user'
+
+RSpec.describe User do
+    describe ".ban_user" do
+        before(:each){@user=User.new(1, "Test", "test@test.com", "user", false)}
+        #before(:each){get :ban_user, {:uid=>@user.id}}
+        it "ban a user" do
+            #id=params[:uid]
+            id=@user.id
+            allow(User).to receive(:find).with(id).and_return(@user)
+            @user.ban_user
+            expect(@user.banned).to eq(true)
+            #allow(@user).to receive(:update).and_return(true)
+            #allow(@user).to receive(:save).and_return(true)
+        end
+    end
+end
